699645862aca8e4e2e3171726bb24d84ee7ffec3
hyun <coderhyun476@gmail.com>
2022-01-30 23:14:53 +0900

    ch6

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,60 @@
 import {https} from "firebase-functions";
-import {HttpsError} from "firebase-functions/v1/https";
+import Spotify from "spotify-web-api-node";
 
-export const isRunning = https.onCall(async (data, context) => {
-  // 데이터 없으면 차단
-  if (!data) throw new HttpsError("data-loss", "데이터가 없습니다.");
-  // 비로그인 차단
-  if (!context.auth)
-    throw new HttpsError("unauthenticated", "로그인이 필요한 작업입니다.");
-  // 비동기 테스트를 위한 1초 딜레이
-  await new Promise(res => setTimeout(res, 1000));
-
-  return "server is running";
+const spotify = new Spotify({
+  clientId: "9ed1177dd6a4429db6fd2a025cb8ffb1",
+  clientSecret: "97081903623b4ffb8654043f8c8d553e",
+  redirectUri: "http://localhost/callback",
 });
+
+export const getSpotifyOAuthUrl = https.onCall(async () => {
+  const url = spotify.createAuthorizeURL(["user-read-email"], "");
+  return url;
+});
+
+import _admin from "firebase-admin";
+import {UserRecord} from "firebase-functions/v1/auth";
+
+export const admin =
+  process.env.NODE_ENV === "test" // 테스트 환경이라면
+    ? _admin.initializeApp({
+        // 로컬에서 인증서를 가져옴
+        credential: _admin.credential.cert(
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          require("../testServiceAccountKey.json"),
+        ),
+      })
+    : // 테스트 환경이 아니라면 firebase의 명령어로 실행하기에 자동으로 초기화됨
+      _admin.initializeApp();
+
+export const getSpotifyFirebaseCustomToken = https.onCall(
+  async (data: {spotifyCode: string}) => {
+    const {spotifyCode} = data;
+
+    // 로그인 토큰을 받아와서 accessToken과 refreshToken으로 변환함
+    const credential = await spotify.authorizationCodeGrant(spotifyCode);
+    // 유저 세부 정보를 가져옴
+    spotify.setAccessToken(credential.body.access_token); // getMe() accessToken을 기준으로 정보를 가져오기 때문에 적용
+    const me = await spotify.getMe();
+
+    const uid = me.body.id; // firebase auth에서 id로 사용할 값
+    const email = me.body.email; // firebase auth에 저장할 이메일
+
+    let user: UserRecord;
+
+    try {
+      // 이미 유저가 있다면 내용을 업데이트함
+      user = await admin.auth().updateUser(uid, {email});
+    } catch (_error) {
+      // 유저가 없다는 내용의 에러인지 확인
+      const error = _error as any;
+      if (error.errorInfo.code !== "auth/user-not-found") throw error;
+      // 없다면 유저를 생성함
+      user = await admin.auth().createUser({uid, email});
+    }
+
+    // 앱에서 firebase auth를 사용하여 로그인을 관리할 수 있도록 firebase token을 생성
+    const token = await admin.auth().createCustomToken(user.uid);
+    return token;
+  },
+);
--- functions/test/index.test.ts
+++ functions/test/index.test.ts
@@ -1,77 +1,61 @@
 import {expect} from "chai";
 import firebaseFunctionsTest from "firebase-functions-test";
-import {auth} from "firebase-functions/v1";
+import puppeteer from "puppeteer";
 
-// onCall 함수를 실행해주는 모듈
 const testFunctions = firebaseFunctionsTest();
 
-// description은 그룹화 라고 보면됨, 보통 "/", "/api/auth"등 폴더, 라우팅 위치등을 title로 사용함
 describe("/", () => {
   let Functions: any;
-  let testUser: auth.UserRecord;
+  let loginUrl: string;
+  let token: string;
 
-  // "/"이 스코프 안에 테스트 함수들이 실행되기 전 가장먼저 실행되는 함수, 생상자 같은 느낌
   before(() => {
-    // import를 사용하지 않고 require을 사용하는 이유는
-    // import로 불러오면 "firebaseFunctionsTest()"이 함수등
-    // 기본적으로 필요한 setup들이 있는데 그보다 전에 호출이 되어 test환경이 아닌 dev환경으로 인식됨
-    // 다음 챕터에서 확인할 수 있음
     Functions = require("../src/index.ts");
-    // 임의의 테스트 유저 생성
-    testUser = testFunctions.auth.makeUserRecord({id: "test_user"});
   });
 
-  // 이 스코프 마지막함수 실행후에 실행되는 함수, 소멸자 같은 느낌
-  after(async () => {
-    // testFunctions 초기화
-    await testFunctions.cleanup();
+  context("getSpotifyOAuthUrl", () => {
+    it("스포티파이로 로그인 할 수 있는 주소를 반환합니다.", async () => {
+      const result = await testFunctions.wrap(Functions.getSpotifyOAuthUrl)({});
+      loginUrl = result; // 웹뷰에서 띄우기 위해 변수로 저장해둠
+      expect(result).to.be.a("string"); // 문자열로 반환하는지 테스트
+      expect(result).to.include("https://accounts.spotify.com/authorize"); // 유효한 주소인지 확인
+    });
   });
 
-  // context는 description과 일치 하는 기능을 갖지만 주로 실제 실행할 함수의 이름을 title로 사용한다.
-  context("isRunning", () => {
-    // it는 실제 테스트함수
-    it("결과의 타입은 string입니다.", async () => {
-      // 실행할 함수를 testFunctions.wrap로 감싸면 실행할 수 있다.
-      const result = await testFunctions.wrap(Functions.isRunning)(
-        // Data
-        {
-          testData: "hello",
-        },
-        // Context
-        {auth: testUser},
+  context("실제 로그인 테스트", () => {
+    it("웹뷰로 로그인 중...", async () => {
+      // 브라우저 실행
+      const browser = await puppeteer.launch({
+        headless: false, // 백그라운드에서 웹뷰를 띄울지, 실제 배포시에는 true로 변경
+        timeout: 15000,
+      });
+      const page = await browser.newPage(); // 페이지 하나 생성
+      await page.goto(loginUrl); // getSpotifyOAuthUrl에서 받은 url로 이동
+      await page.type("#login-username", "musicshortsvelog@gmail.com"); // 아이디 input에 테스트계정 아이디 삽입
+      await page.type("#login-password", "musicshorts12!@"); // 페스워드 input에 비밀번호 삽입
+      await page.waitForTimeout(1000); // 삽입된데이터 적용시간
+      await page.click("#login-button"); // 로그인 작동
+      await new Promise<void>(res =>
+        page.on("response", event => {
+          // 302 리다이렉트 요청이 왓을때
+          if (event.status() === 302) {
+            // 리다이렉트할 주소의 url query에 "code"를 가져와 token 저장
+            token = event.headers().location.split("code=")[1].split("&")[0];
+            res(); // 프로미스 탈출
+          }
+        }),
       );
-      // a()함수는 타입체크
-      expect(result).to.be.a("string");
-      // console.log(result) <= 이러면 안됨
-      // async 함수안에서 test할때는 함수의 마지막 줄이 무조건 expect여야함 아닐시 timeout에러 발생
-    });
+      await browser.close(); // 브라우저 닫기
+      expect(token.length).to.be.greaterThanOrEqual(1); // 유효한 토큰인지 테스트
+    }).timeout(15000);
+  });
 
-    it("결과값은 server is running 입니다.", async () => {
-      const result = await testFunctions.wrap(Functions.isRunning)(
-        {
-          testData: "hello",
-        },
-        {auth: testUser},
-      );
-      expect(result).to.equal("server is running");
-    });
-
-    it("Data가 없으면 오류가 발생시킵니다.", async () => {
-      // 에러 채크는 .catch후 result를 error로 매핑한다.
-      const result = await testFunctions
-        .wrap(Functions.isRunning)(undefined, {auth: testUser})
-        .catch((e: any) => e);
-      // 그후 타입이 error인지 체크
-      expect(result).to.be.a("error");
-    });
-
-    it("비로그인시 오류가 발생합니다.", async () => {
-      const result = await testFunctions
-        .wrap(Functions.isRunning)({
-          testData: "hello",
-        })
-        .catch((e: any) => e);
-      expect(result).to.be.a("error");
-    });
+  context("getSpotifyFirebaseCustomToken", () => {
+    it("함수 호출시 파이어베이스 토큰을 반환합니다.", async () => {
+      const result = await testFunctions.wrap(
+        Functions.getSpotifyFirebaseCustomToken,
+      )({spotifyCode: token});
+      expect(result).to.be.a("string"); // 토큰을 반환하는지
+    }).timeout(5000);
   });
 });
